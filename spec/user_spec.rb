fa78c76a5a05fe9266bd7965b1de798c8796e399
Kiran Hare <kiran.hare@itv.com>
2014-03-25 15:31:00 +0000

    Finished tests for session 5 rspec framework

---
+++ spec/user_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe User do
+
+		let(:user) { User.new }
+
+	it { should respond_to(:age) }
+	it { should respond_to(:name) }
+	it { should respond_to(:roles) }
+	it { should respond_to(:address) }
+	
+	it "should return admin" do
+		expect(user).to be_admin
+	end
+2
+	it "should return the name 'bob'" do
+		expect(user.name).to eq "Bob"
+	end
+
+	it "should match the postcode to the address" do
+		user.postcode = 'WC1 9XC'
+		expect(user.address).to match user.postcode
+	end
+
+	it "should return as error if role added is empty" do
+		expect { user.add_role() }.to raise_error ArgumentError
+	end
+end
